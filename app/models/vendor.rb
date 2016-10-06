6e96cb0f309bca8768a4f5fe1c794af10c96db7a
Slobodan <slobodan.stevanovic@puhsd.org>
2016-10-06 15:10:19 -0700

    Views have been modified

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,7 +1,7 @@
 class Vendor < ApplicationRecord
   has_many :passwords;
   has_many :uploads;
-  has_attached_file :logo, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :logo, styles: { medium: "300x300>", thumb: "150x150>" }
   validates_attachment_content_type :logo, content_type: /\Aimage\/.*\z/
 
 end
